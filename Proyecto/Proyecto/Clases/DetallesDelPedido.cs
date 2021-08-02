90410a6b47b62b748699b8eecf53c3337bddde1d
UtuCastillos <utucastillos2021@gmail.com>
2021-08-02 20:48:51 -0300

    Esta es la primer version del software.

---
+++ Proyecto/Proyecto/Clases/DetallesDelPedido.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto.Clases
+{
+    class DetallesDelPedido
+    {
+        private int ordenId;
+        private int productId;
+        private string nombre;
+        private int cantidad;
+        private float total;
+
+        public DetallesDelPedido(int ordenId, int productId, string nombre, int cantidad, float total)
+        {
+
+            this.ordenId = ordenId;
+            this.productId = productId;
+            this.nombre = nombre;
+            this.cantidad = cantidad;
+            this.total = total;
+        }
+
+        public int calcularPrecio()
+        {
+
+            return 0;
+        }
+    }
+
+}
---
+++ Proyecto/Proyecto/Interfaces/AppGeneral.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proyecto.Interfaces
+{
+    public partial class AppGeneral : Form
+    {
+        public AppGeneral()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_CerrarSesion_Click(object sender, EventArgs e)
+        {
+            //Creo un Objeto (Formulario) que se guarde como "cerrarSesion"
+            Form cerrarSesion = new Login();
+            //Oculto la ventana actual y abro la otra (que seria Login)
+            this.Hide();
+            cerrarSesion.Show();
+
+        }
+
+        private void btn_MiCuenta_Click(object sender, EventArgs e)
+        {
+            Form MiCuenta = new Perfil();
+            this.Hide();
+            MiCuenta.Show();
+        }
+
+        private void btn_perfil_Click(object sender, EventArgs e)
+        {
+            Form MiCuenta = new Perfil();
+            this.Hide();
+            MiCuenta.Show();
+        }
+
+        private void btn_perfil_Click_1(object sender, EventArgs e)
+        {
+            Form MiCuenta = new Perfil();
+            this.Hide();
+            MiCuenta.Show();
+        }
+    }
+}
